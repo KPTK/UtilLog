25ce32f9f475f456b8c876d8d8c00311b96b1339
KPTK <kevinkowalski89@gmail.com>
2021-01-05 23:19:30 -0600

    update the lib aar

--- app/src/main/java/com/alpha/logutilexample/MainActivity.java
+++ app/src/main/java/com/alpha/logutilexample/MainActivity.java
@@ -3,12 +3,21 @@ package com.alpha.logutilexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.alpha.logutil.RandIntGenerator;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int randint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int x = RandIntGenerator.getRandInt();
+
+        Log.d("MainActivity", "onCreate: "+ x);
     }
 }
